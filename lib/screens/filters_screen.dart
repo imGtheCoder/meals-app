83aaeebe15ca4baa0227c0e9071ce812deb78141
imGtheCoder <stiopimanol34@gmail.com>
2023-01-12 20:32:57 +0200

    Passing data backwards

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false
 
   @override
   Widget build(BuildContext context) {
